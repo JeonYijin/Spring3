e1bcf585ce6f2ee8f771a9c304ef04bbbb05fe9f
JeonYijin <juan6016@naver.com>
2021-08-30 17:52:04 +0900

    8.30-2

---
+++ src/main/java/com/ae/ae1/bankbook/BankbookController.java
@@ -0,0 +1,28 @@
+package com.ae.ae1.bankbook;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/bankbook/*")
+public class BankbookController {
+	//pojo(plain old java object)
+	
+	@RequestMapping(value = "bankbookList.do", method= RequestMethod.GET)
+	public String list() {
+		System.out.println("bankbook List");
+		return "bankbook/bankbookList";
+	}
+	
+	@RequestMapping("bankbookSelect.do")
+	public String select(int num, String name) {
+		System.out.println("value: "+num);
+		System.out.println("Name: " + name);
+		return "bankbook/bankbookSelect";
+	}
+	
+	
+}
