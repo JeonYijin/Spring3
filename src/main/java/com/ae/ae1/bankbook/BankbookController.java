943ac081e2974e0ed00d14915c8f88c182c15a36
JeonYijin <juan6016@naver.com>
2021-09-02 09:50:19 +0900

    9.2

--- src/main/java/com/ae/ae1/bankbook/BankbookController.java
+++ src/main/java/com/ae/ae1/bankbook/BankbookController.java
@@ -2,14 +2,10 @@ package com.ae.ae1.bankbook;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
